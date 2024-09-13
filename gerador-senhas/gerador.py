b60427b19de8be298d5871148dcc22c0527e4192
Gabi <gabrielamalerba10@gmail.com>
2024-09-13 14:29:05 -0300

    finished :)))

--- gerador-senhas/gerador.py
+++ gerador-senhas/gerador.py
@@ -30,80 +30,80 @@ def main(page: ft.Page):
         page.update()
 
 #Dunção para copiar a senha gerada para a área de transferência
-def copiar_senha(e):
-    page.set_clipboard(senha_output.value)
-    snack = ft. SnackBar(ft.Text("Senha copiada para a área de transferência!"))
-    page.overlay.appen(snack)
-    snack.open = TRue
-    page.update()
+    def copiar_senha(e):
+        page.set_clipboard(senha_output.value)
+        snack = ft. SnackBar(ft.Text("Senha copiada para a área de transferência!"))
+        page.overlay.appen(snack)
+        snack.open = True
+        page.update()
 
 #Elemento de título da UI, com estilo e espaçamento no topo
-titulo = ft.Container(
-    content=ft.Text("Gerador de Senhas", size=28, weight="bold"),
-    padding=ft.padding.only(top=50)
-)
+    titulo = ft.Container(
+        content=ft.Text("Gerador de Senhas", size=28, weight="bold"),
+        padding=ft.padding.only(top=50)
+    )
 
 #Campo de texto que mostra a senha gerada, com ícone de copiar e um fundo variante
-senha_output = ft.TextField(
-    value="",
-    label="Senha Gerada",
-    read_only=True,
-    width=280,
-    suffix=ft.IconButton(ft.icons.COPY, on_click=copiar_senha),
-    bgcolor=ft.colors.SURFACE_VARIANT
-)
+    senha_output = ft.TextField(
+        value="",
+        label="Senha Gerada",
+        read_only=True,
+        width=280,
+        suffix=ft.IconButton(ft.icons.COPY, on_click=copiar_senha),
+        bgcolor=ft.colors.SURFACE_VARIANT
+    )
 
 #Slider que pertmite escolher o complemento da senha (de 8 a 20 caracteres)
-slider = ft.Slider(
-    min=8,
-    max=20,
-    value=12,
-    divisions=12,
-    label="CARACTERES: {value}"
-)
+    slider = ft.Slider(
+        min=8,
+        max=20,
+        value=12,
+        divisions=12,
+        label="CARACTERES: {value}"
+    )
 
 #Switch para selecionar se letras maiúsculas devem ser incluídas na senha
-upper_switch = ft.Switch(label="Letras maiúsculas")
-lower_switch = ft.Switch(label="Letras maiúsculas, value=True")
-numbers_switch = ft.Switch(label="Incluir números")
-symbols_switch = ft.Switch(label="Incluir símbolos")
+    upper_switch = ft.Switch(label="Letras maiúsculas")
+    lower_switch = ft.Switch(label="Letras maiúsculas, value=True")
+    numbers_switch = ft.Switch(label="Incluir números")
+    symbols_switch = ft.Switch(label="Incluir símbolos")
 
 #Botão para gerar a senha, que chama a função gerar_senha ao ser clicado
-gerar_button = ft.ElevatedButton (
-    text="GERAR SENHA",
-    on_click=gerar_senha,
-    color=ft.colors.ON_PRIMARY
-    bgcolor=ft.colors.PRIMARY
-)
+    gerar_button = ft.ElevatedButton(
+        text="GERAR SENHA",
+        on_click=gerar_senha,
+        color=ft.colors.ON_PRIMARY,
+        bgcolor=ft.colors.PRIMARY
+    )
 
 #Coluna para organizar os switches de forma centralizaad
-setores= ft.Column(
-    [
-        upper_switch,
-        lower_switch,
-        numbers_switch,
-        symbols_swith,
-    ],
-    alignment=ft.MainAxisAlignment.CENTER,
-    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-    spacing=10
-)
-
-page.add(
-        ft.Column(
-            [
-                titulo,
-                senha_output,
-                slider,
-                ft.Text("PREFERÊNCIAS"),
-                seletores,
-                gerar_button,
-            ], 
-            alignment=ft.MainAxisAlignment.START,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            spacing=10,
-        )
+    seletores = ft.Column(
+        [
+            upper_switch,
+            lower_switch,
+            numbers_switch,
+            symbols_switch,
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        spacing=10
     )
+
+    page.add(
+            ft.Column(
+                [
+                    titulo,
+                    senha_output,
+                    slider,
+                    ft.Text("PREFERÊNCIAS"),
+                    seletores,
+                    gerar_button,
+                ], 
+                alignment=ft.MainAxisAlignment.START,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=10,
+            )
+        )
 
 #Executa o aplicativo, chamando a função main como ponto de entrada
 ft.app(target=main)
